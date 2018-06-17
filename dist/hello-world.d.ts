97126e7e3cfe3062c12cab7b5d437e0d5c67298a
Yusuke Shinozaki <vin.bleu@gmail.com>
2018-06-17 17:53:52 +0900

    initial commit

---
+++ dist/hello-world.d.ts
@@ -0,0 +1,2 @@
+export declare const hello: () => string;
+//# sourceMappingURL=hello-world.d.ts.map
---
+++ dist/hello-world.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = () => "Hello world!";
+//# sourceMappingURL=hello-world.js.map
